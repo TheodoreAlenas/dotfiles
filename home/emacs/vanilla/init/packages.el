ab6ce2f9b2b2ee81ceb0a62d774082d726d31b38
Theodore Alenas <dimakopt732@gmail.com>
2023-12-20 16:33:23 +0200

    emacs: removed org customization

--- home/emacs/vanilla/init/packages.el
+++ home/emacs/vanilla/init/packages.el
@@ -1,24 +1,11 @@
 (vertico-mode)
-;;(vertico-grid-mode)
-
 (marginalia-mode)
+;;(vertico-grid-mode)
 
 ; got to hook it better
 (editorconfig-mode)
 
 (setq eglot-confirm-server-initiated-edits nil)
-
-(setq org-hide-emphasis-markers t)
-
-;; (custom-theme-set-faces
-;;  'user
-;;  `(org-level-4 ((t (:font "Source Code Pro" :height 1.1))))
-;;  `(org-level-3 ((t (:font "Source Code Pro" :height 1.25))))
-;;  `(org-level-2 ((t (:font "Source Code Pro" :height 1.5))))
-;;  `(org-level-1 ((t (:font "Source Code Pro" :height 1.75))))
-;;  `(org-document-title ((t (:font "Source Code Pro" :height 2.0 :underline nil)))))
-;;
-;; autosaves
 
 (require 'git-gutter)
 (global-git-gutter-mode 1)
@@ -30,7 +17,7 @@
 ;; (eval-after-load *tetris* (define-key tetris-mode-map (kbd "<up>") 'tetris-rotate-next))
 
 (defun al/prepare-for-ligatures ()
-  "To Fira Code, for Haskell (ligatures are from a package)"
+  "For Haskell (ligatures are from a package)"
   (set-face-attribute 'default nil :font "Hasklug Nerd Font"))
 
 (use-package haskell-mode :config

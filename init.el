db91b49876a37db8822282e94ba091180a37a54b
Theodore Alenas <dimakopt732@gmail.com>
2024-01-16 18:42:06 +0200

    org mode got variable pitch outside blocks

--- init.el
+++ init.el
@@ -19,7 +19,8 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (toggle-scroll-bar -1)
-(set-face-attribute 'default nil :font "Source Code Pro-15")
+(set-face-attribute 'default nil :family "Source Code Pro" :height 150)
+(set-face-attribute 'fixed-pitch nil :family "Source Code Pro" :height 150)
 (set-frame-parameter nil 'alpha-background 99)
 (add-to-list 'default-frame-alist '(alpha-background . 99))
 
@@ -34,8 +35,12 @@
 
 ;; org
 (require 'org)
-;; the following line doesn't function
-(add-to-list 'org-mode-hook '(variable-pitch-mode 1))
+(add-hook 'text-mode-hook #'variable-pitch-mode)
+;;(setq org-src-block-faces 'fixed-pitch)
+(set-face-attribute 'org-table       nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-agenda-date nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-date        nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-block       nil :inherit 'fixed-pitch)
 
 ;; eglot
 (setq eglot-confirm-server-initiated-edits nil)

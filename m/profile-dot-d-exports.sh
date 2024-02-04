c1df778dce0439290fe1fabba58202b3a21b831d
Theodore Alenas <dimakopt732@gmail.com>
2024-02-04 18:46:14 +0200

    XDG exports gone

--- m/profile-dot-d-exports.sh
+++ m/profile-dot-d-exports.sh
@@ -1,8 +1,3 @@
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_STATE_HOME="$HOME/.local/state"
-export XDG_DATA_HOME="$HOME/.local/share"
-
 # sourcing this file multiple times won't lengthen $PATH
 export AL_PATH_FROM_ROOT="${AL_PATH_FROM_ROOT:-"$PATH"}"
 AL_PATH_1=~/.local/bin

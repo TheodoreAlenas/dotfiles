02925daf3c3bafe199efeab8c888efe113ccac61
Theodore Alenas <dimakopt732@gmail.com>
2024-03-01 06:48:06 +0200

    GOPATH, GOBIN

--- tangled/profile.d-01-all.sh
+++ tangled/profile.d-01-all.sh
@@ -1,8 +1,11 @@
+GOPATH=~/.local/share/go
+GOBIN=~/.local/share/go/bin
+
 # sourcing this file multiple times won't lengthen $PATH
 export AL_PATH_FROM_ROOT="${AL_PATH_FROM_ROOT:-"$PATH"}"
 AL_PATH_1=~/.local/bin
 AL_PATH_2=~/.local/share/nvim/mason/bin
-export PATH="$AL_PATH_1:$AL_PATH_2:$AL_PATH_FROM_ROOT"
+export PATH="$AL_PATH_1:$AL_PATH_2:$GOBIN:$AL_PATH_FROM_ROOT"
 
 export EDITOR=nvim
 export DEFAULT_TERM=alacritty

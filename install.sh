e6d1ed416a50c5a9eb481eb44d56f2da46865828
Theodore Alenas <dimakopt732@gmail.com>
2024-11-10 21:20:47 +0200

    mv tangled -> other

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ for_each_src_dest() {
     "$@" bash-and-zsh/dot-bashrc.bash ~/.bashrc
     "$@" bash-and-zsh/dot-zshrc.zsh ~/.zshrc
 
-    "$@" tangled/mpv.conf ~/.config/mpv/mpv.conf
-    "$@" tangled/profile.d-01-all.sh ~/.config/profile.d/01-all.sh
-    "$@" tangled/tmux.conf ~/.config/tmux/tmux.conf
-    "$@" tangled/bspwmrc ~/.config/bspwm/bspwmrc
-    "$@" tangled/bspwm-theme ~/.local/bin/bspwm-theme
-    "$@" tangled/dot-xinitrc ~/.xinitrc
-    "$@" tangled/dot-Xresources ~/.Xresources
-    "$@" tangled/dot-Xresources ~/.Xdefaults
+    "$@" other/mpv.conf ~/.config/mpv/mpv.conf
+    "$@" other/profile.d-01-all.sh ~/.config/profile.d/01-all.sh
+    "$@" other/tmux.conf ~/.config/tmux/tmux.conf
+    "$@" other/bspwmrc ~/.config/bspwm/bspwmrc
+    "$@" other/bspwm-theme ~/.local/bin/bspwm-theme
+    "$@" other/dot-xinitrc ~/.xinitrc
+    "$@" other/dot-Xresources ~/.Xresources
+    "$@" other/dot-Xresources ~/.Xdefaults
 }
 
 ln_full_path() { ln -sv "$PWD/$1" "$2"; }
@@ -22,5 +22,5 @@ rm_dest() { rm -fv "$2"; }
 case "$1" in
     (ln) for_each_src_dest ln_full_path ;;
     (rm) for_each_src_dest rm_dest ;;
-    (*) echo "$0 ln   or   $0 rm" >&2 ; exit 1 ;;
+    (*) echo "$0 ln" ; echo "$0 rm" >&2 ; exit 1 ;;
 esac
